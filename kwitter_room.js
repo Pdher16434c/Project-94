3ca1259b9b82b16c6e1d9b589604c6e22f999213
Pdher16434c <86463739+Pdher16434c@users.noreply.github.com>
2021-08-13 11:07:21 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyALlebPOx0MNF1qOaEo7nqTyFwMnVhuFIo",
+    authDomain: "kwiter-33835.firebaseapp.com",
+    databaseURL: "https://kwiter-33835-default-rtdb.firebaseio.com",
+    projectId: "kwiter-33835",
+    storageBucket: "kwiter-33835.appspot.com",
+    messagingSenderId: "378564276438",
+    appId: "1:378564276438:web:282a44f067ca36b542d977"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
